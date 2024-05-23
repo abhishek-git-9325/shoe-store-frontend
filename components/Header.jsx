042d283e8770dadac2bbe782ce2e22cdb3078f88
abhishek-git-9325 <abhishek.mourya@amuratech>
2024-05-23 19:07:53 +0530

    Wishlist icon removed from header

--- components/Header.jsx
+++ components/Header.jsx
@@ -1,106 +1,113 @@
-import React, { useEffect, useState } from 'react'
-import Wrapper from './Wrapper';
-import Link from 'next/link';
-import Menu from './Menu';
+import React, { useEffect, useState } from "react";
+import Wrapper from "./Wrapper";
+import Link from "next/link";
+import Menu from "./Menu";
 
-import { IoMdHeartEmpty } from 'react-icons/io';
-import { BsCart } from 'react-icons/bs';
-import { BiMenuAltRight } from 'react-icons/bi';
-import { VscChromeClose } from 'react-icons/vsc';
-import MenuMobile from './MenuMobile';
-import fetchDataFromAPI from '@/utils/api';
-import { useSelector } from 'react-redux';
+import { IoMdHeartEmpty } from "react-icons/io";
+import { BsCart } from "react-icons/bs";
+import { BiMenuAltRight } from "react-icons/bi";
+import { VscChromeClose } from "react-icons/vsc";
+import MenuMobile from "./MenuMobile";
+import fetchDataFromAPI from "@/utils/api";
+import { useSelector } from "react-redux";
 
 const Header = () => {
-    const [mobileMenu, setMobileMenu] = useState(false);
-    const [showCatMenu, setShowCatMenu] = useState(false);
-    const [show, setShow] = useState('translate-y-0');
-    const [lastScrollY, setLastScrollY] = useState(0);
-    const [categories, setCategories] = useState(null);
-    const { cartItems } = useSelector(state => state.cart);
+  const [mobileMenu, setMobileMenu] = useState(false);
+  const [showCatMenu, setShowCatMenu] = useState(false);
+  const [show, setShow] = useState("translate-y-0");
+  const [lastScrollY, setLastScrollY] = useState(0);
+  const [categories, setCategories] = useState(null);
+  const { cartItems } = useSelector((state) => state.cart);
 
-    const controlNavbar = () => {
-        const currentScrollY = window.scrollY;
-        if (currentScrollY > 200) {
-            if (currentScrollY > lastScrollY && !mobileMenu) {
-                setShow('-translate-y-full');
-            } else {
-                setShow('shadow-sm');
-            }
-        } 
-        else {
-            setShow('translate-y-0');
-        }
-        setLastScrollY(currentScrollY);
+  const controlNavbar = () => {
+    const currentScrollY = window.scrollY;
+    if (currentScrollY > 200) {
+      if (currentScrollY > lastScrollY && !mobileMenu) {
+        setShow("-translate-y-full");
+      } else {
+        setShow("shadow-sm");
+      }
+    } else {
+      setShow("translate-y-0");
     }
+    setLastScrollY(currentScrollY);
+  };
 
-    useEffect(() => {
-        window.addEventListener('scroll', controlNavbar);
-        return () => {
-            window.removeEventListener('scroll', controlNavbar);
-        }
-    },[lastScrollY]);
+  useEffect(() => {
+    window.addEventListener("scroll", controlNavbar);
+    return () => {
+      window.removeEventListener("scroll", controlNavbar);
+    };
+  }, [lastScrollY]);
 
-    useEffect(() => {
-        fetchCategories();
-    },[]);
-    
-    const fetchCategories = async () => {
-        const { data } = await fetchDataFromAPI('/api/categories?populate=*');
-        setCategories(data);
-    }
+  useEffect(() => {
+    fetchCategories();
+  }, []);
+
+  const fetchCategories = async () => {
+    const { data } = await fetchDataFromAPI("/api/categories?populate=*");
+    setCategories(data);
+  };
   return (
-    <header className={`w-full h-[50px] md:h-[80px] bg-white flex items-center justify-between z-20 sticky top-0 transition-transform duration-300 ${show}`}>
-        <Wrapper className="h-[60px] flex justify-between items-center">
-            <Link href='/'>
-                <img src='/logo.svg' alt='logo' className='w-[40px] md:w-[60px]' />
-            </Link>
-            <Menu 
-                showCatMenu={showCatMenu} 
-                setShowCatMenu={setShowCatMenu}
-                categories={categories} 
-            />
-            {mobileMenu && 
-                <MenuMobile 
-                    showCatMenu={showCatMenu} 
-                    setShowCatMenu={setShowCatMenu} 
-                    setMobileMenu={setMobileMenu}
-                    categories={categories}
-                />
-            }
-            <div className="flex items-center gap-2 text-black">
-                {/* Wishlist */}
-                <div className="w-8 md:w-12 h-8 md:h-12 rounded-full flex justify-center items-center hover:bg-black/[0.05] cursor-pointer relative">
+    <header
+      className={`w-full h-[50px] md:h-[80px] bg-white flex items-center justify-between z-20 sticky top-0 transition-transform duration-300 ${show}`}
+    >
+      <Wrapper className="h-[60px] flex justify-between items-center">
+        <Link href="/">
+          <img src="/logo.svg" alt="logo" className="w-[40px] md:w-[60px]" />
+        </Link>
+        <Menu
+          showCatMenu={showCatMenu}
+          setShowCatMenu={setShowCatMenu}
+          categories={categories}
+        />
+        {mobileMenu && (
+          <MenuMobile
+            showCatMenu={showCatMenu}
+            setShowCatMenu={setShowCatMenu}
+            setMobileMenu={setMobileMenu}
+            categories={categories}
+          />
+        )}
+        <div className="flex items-center gap-2 text-black">
+          {/* Wishlist */}
+          {/* <div className="w-8 md:w-12 h-8 md:h-12 rounded-full flex justify-center items-center hover:bg-black/[0.05] cursor-pointer relative">
                     <IoMdHeartEmpty size={24} className='text-[19px] md:text-[24px]' />
                     <div className='h-[14px] md:h-[18px] min-w-[14px] md:min-w-[18px] rounded-full bg-red-600 absolute top-1 left-5 md:left-7 text-white text-[10px] md:text-[12px] font-semibold flex justify-center items-center px-[2px] md:px-[5px]'>5</div>
-                </div>
+                </div> */}
 
-                {/* Cart */}
-                <Link href='/cart'>
-                <div className="w-8 md:w-12 h-8 md:h-12 rounded-full flex justify-center items-center hover:bg-black/[0.05] cursor-pointer relative">
-                    <BsCart size={24} className='text-[15px] md:text-[20px]' />
-                    { cartItems?.length > 0 &&
-                    <div className='h-[14px] md:h-[18px] min-w-[14px] md:min-w-[18px] rounded-full bg-red-600 absolute top-1 left-5 md:left-7 text-white text-[10px] md:text-[12px] font-semibold flex justify-center items-center px-[2px] md:px-[5px]'>
-                        {
-                            cartItems?.length
-                        }
-                    </div>
-                    }
+          {/* Cart */}
+          <Link href="/cart">
+            <div className="w-8 md:w-12 h-8 md:h-12 rounded-full flex justify-center items-center hover:bg-black/[0.05] cursor-pointer relative">
+              <BsCart size={24} className="text-[15px] md:text-[20px]" />
+              {cartItems?.length > 0 && (
+                <div className="h-[14px] md:h-[18px] min-w-[14px] md:min-w-[18px] rounded-full bg-red-600 absolute top-1 left-5 md:left-7 text-white text-[10px] md:text-[12px] font-semibold flex justify-center items-center px-[2px] md:px-[5px]">
+                  {cartItems?.length}
                 </div>
-                </Link>
-
-                {/* MobileMenu */}
-                <div className="flex w-8 md:w-12 h-8 md:h-12 rounded-full md:hidden justify-center items-center hover:bg-black/[0.05] cursor-pointer relative -mr-2">
-                    {mobileMenu ? (
-                        <VscChromeClose className='text-[16px]' size={24} onClick={()=> setMobileMenu(false)} />
-                    ):(
-                        <BiMenuAltRight className='text-[20px]' size={24} onClick={()=> setMobileMenu(true)} />
-                    )}
-                </div>
+              )}
             </div>
-        </Wrapper>
-    </header>
-  )
-}
+          </Link>
 
-export default Header
+          {/* MobileMenu */}
+          <div className="flex w-8 md:w-12 h-8 md:h-12 rounded-full md:hidden justify-center items-center hover:bg-black/[0.05] cursor-pointer relative -mr-2">
+            {mobileMenu ? (
+              <VscChromeClose
+                className="text-[16px]"
+                size={24}
+                onClick={() => setMobileMenu(false)}
+              />
+            ) : (
+              <BiMenuAltRight
+                className="text-[20px]"
+                size={24}
+                onClick={() => setMobileMenu(true)}
+              />
+            )}
+          </div>
+        </div>
+      </Wrapper>
+    </header>
+  );
+};
+
+export default Header;
